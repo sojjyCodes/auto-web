0774285eed4034be0bf2c24bc048797654de0c72
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-07-03 18:15:24 +0100

    added a few things

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+#Created by sojjyCodes.
 from selenium import webdriver
+import pyttsx3
+import time
 operation = input('''
 what browser do you want to use to perform this task 
 1 for Chrome
@@ -6,11 +9,20 @@ what browser do you want to use to perform this task
 ''')
 
 if operation == '1':
-
+    print("Opening Chrome ")
+    time.sleep(3)
+    pyttsx3.speak("Opening, Chrome")
     driver = webdriver.Chrome()
     driver.get("https://www.github.com/sojjyCodes")
 
-else:
-    if operation == '2':
+elif operation == '2':
+        print("Opening Firefox")
+        time.sleep(3) 
+        pyttsx3.speak("Opening Firefox")
         driver2 = webdriver.Firefox()
         driver2.get("https://www.facebook.com")
+
+else:
+    pyttsx3.speak("Invalid,inpute") #Spelling "inpute" was  intentional. if he wa to speak input it will come out wrong.
+    print("Invalid input")
+
